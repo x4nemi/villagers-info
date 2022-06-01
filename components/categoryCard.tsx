7b1e47095c4c3a5cff674f2750bac3f268bebbe0
Ximena <ximena.garcia7482@alumnos.udg.mx>
2022-05-31 22:54:38 -0500

    Category filter added

---
+++ components/categoryCard.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface CategoryProps {
+  Icon: string;
+  group: string;
+  handleFilter: (group: string) => void;
+}
+
+const CategoryCard: React.FunctionComponent<CategoryProps> = ({
+  Icon,
+  group,
+  handleFilter,
+}) => {
+  return (
+    <React.Fragment>
+      <button onClick={() => handleFilter(group)}>
+        <span className="flex justify-start items-center pl-7 rounded-md bg-gray-50 py-2 hover:bg-gray-100">
+          <span>
+            <img className="w-16 md:w-32 lg:w-full self-baseline" src={Icon} />
+          </span>
+          <span>{group}</span>
+        </span>
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default CategoryCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 import List from "../components/list";
 import NeighborCard from "../components/neighborCard";
 import neighborsData from "../data/neighbors";
+import CategoryCard from "../components/categoryCard";
 
 const Home: NextPage = () => {
   const [filteredNeighbors, setFilteredNeighbors] =
@@ -12,26 +13,76 @@ const Home: NextPage = () => {
   function handleFilterBySearch(search: string) {
     if (search.length > 0) {
       setFilteredNeighbors(
-        neighborsData.filter((neighbor) => {
-          return neighbor.Name.toLowerCase().includes(search.toLowerCase());
-        })
+        neighborsData
+          .filter((neighbor) => {
+            return neighbor.Name.toLowerCase().includes(search.toLowerCase());
+          })
+          .slice(0, 100)
       );
     } else {
-      setFilteredNeighbors(neighborsData);
+      setFilteredNeighbors(neighborsData.slice(0, 100));
     }
   }
+
+  function handleFilterByCategory(search: string) {
+    setFilteredNeighbors(
+      neighborsData.filter((neighbor) => {
+        return neighbor.Personality.toLowerCase().includes(
+          search.toLowerCase()
+        );
+      })
+    );
+  }
+
   return (
     <Layout>
       <h1 className="text-2xl md:text-5xl font-bold text-emerald-300 text-center tracking-wide">
         Welcome to the <span className="text-cyan-400">Neighborhood</span>{" "}
       </h1>
-      <div className="justify-center">
+      <div className="flex flex-row justify-center">
         <input
           className="w-full max-w-xl border  border-gray-300 p-3 rounded-md my-8 bg-white shadow-md focus:outline-none focus:border-yellow-200 focus:ring-1"
           id="Name"
           type={"text"}
           placeholder="Search for a villager!"
           onChange={(e) => handleFilterBySearch(e.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-3 mb-8">
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-pride.png"
+          group={"Snooty"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-resignation.png"
+          group={"Cranky"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-flourish.png"
+          group={"Peppy"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-mischief.png"
+          group={"Smug"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-sleepy.png"
+          group={"Lazy"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-work-out.png"
+          group={"Normal"}
+          handleFilter={handleFilterByCategory}
+        />
+        <CategoryCard
+          Icon="https://animalcrossingworld.com/wp-content/uploads/2020/06/animal-crossing-new-horizons-guide-reaction-icon-pride.png"
+          group={"Jock"}
+          handleFilter={handleFilterByCategory}
         />
       </div>
       <List neighbors={filteredNeighbors} />
